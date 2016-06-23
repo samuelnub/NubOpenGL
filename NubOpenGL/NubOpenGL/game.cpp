973eaa700b013407a59c015b0d3710177a464552
samuelnub <notsamnub@gmail.com>
2016-06-23 15:48:35 +0800

    creates instance of render object that renders a triangle

--- NubOpenGL/NubOpenGL/game.cpp
+++ NubOpenGL/NubOpenGL/game.cpp
@@ -1,13 +1,37 @@
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>         /// remove the "3" for OpenGL versions < 3
+#include <OpenGL/gl3ext.h>      /// ditto
+#else 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#endif
+
 #include "game.h"
 #include "globals.h"
+#include "render.h"
 #include <iostream>
+
+//temp includes for basic const char*-based glsl shaders, and the vertices of a triangle more basic than my mother
+#include "shadersTest.h"
+#include "verticesTest.h"
 
 Game::Game(GLFWwindow *windu)
 {
+
+	Render triangle(verts::triangle1);
+
+	triangle.bind();
+	triangle.shade(shaders::vertex, shaders::fragment);
+
 	while (!glfwWindowShouldClose(windu))
 	{
 		glfwPollEvents();
 		glfwSwapBuffers(windu);
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		triangle.draw();
 	}
 
 	glfwTerminate();

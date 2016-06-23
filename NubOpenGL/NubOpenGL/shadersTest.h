febfe28e19e077ae31ccd3312e38ee98bba836b4
samuelnub <notsamnub@gmail.com>
2016-06-23 16:04:49 +0800

    retarded const char*-based shaders, temp until my lazy arse gets to implement a class to read glsl shaders from file

---
+++ NubOpenGL/NubOpenGL/shadersTest.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef SHADERSTEST_H
+#define SHADERSTEST_H
+
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>         /// remove the "3" for OpenGL versions < 3
+#include <OpenGL/gl3ext.h>      /// ditto
+#else 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#endif
+
+namespace shaders
+{
+	const GLchar* vertex = "#version 330 core\n"
+		"layout (location = 0) in vec3 position;\n"
+		"void main()\n"
+		"{\n"
+		"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+		"}\0";
+	const GLchar* fragment = "#version 330 core\n"
+		"out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"color = vec4(1.0f, 0.4f, 0.1f, 1.0f);\n"
+		"}\n\0";
+}
+
+#endif

45951f2d6de28082fa4d48583c657747ba900a57
samuelnub <notsamnub@gmail.com>
2016-06-24 21:22:36 +0800

    updated to suit mvp matrices and remove color attribs

--- NubOpenGL/NubOpenGL/resources/shaders/simpleVert.glsl
+++ NubOpenGL/NubOpenGL/resources/shaders/simpleVert.glsl
@@ -1,15 +1,17 @@
 #version 330 core
 
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
-layout(location = 2) in vec2 texCoord;
+layout(location = 1) in vec3 texCoord;
 
 out vec3 ourColor;
 out vec2 TexCoord;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-	gl_Position = vec4(position, 1.0f);
-	ourColor = color;
+	gl_Position = projection * view * model * vec4(position, 1.0f); //its backwards, remember y'dumbo!
 	TexCoord = vec2(texCoord.x, 1.0f - texCoord.y);
 }

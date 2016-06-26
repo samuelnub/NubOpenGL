3aa10415cc25a2a4a0e188ca3180764fea697e67
samuelnub <notsamnub@gmail.com>
2016-06-26 11:44:03 +0800

    implementation of static input class functions that were declared in game.h, as well as initializing it and listening for input

--- NubOpenGL/NubOpenGL/game.cpp
+++ NubOpenGL/NubOpenGL/game.cpp
@@ -12,17 +12,17 @@
 #include "shader.h"
 #include <iostream>
 
-//probably temp, will move matrix operations to a separate class eventually
-#include <glm\glm.hpp>
-#include <glm\gtc\matrix_transform.hpp>
-#include <glm\gtc\type_ptr.hpp>
-
 //temp includes for basic const char*-based glsl shaders, and the vertices of a triangle more basic than my mother
 #include "shadersTest.h"
 #include "verticesTest.h"
 
+
 Game::Game(GLFWwindow *windu)
 {
+	Input::InputSetup(windu);
+
+	G::player.spawn(glm::vec3((0.0f), (0.0f), (5.0f)));
+
 	Render triangolo;
 	Render quaddo;
 	Render cuban;
@@ -35,6 +35,8 @@ Game::Game(GLFWwindow *windu)
 
 	while (!glfwWindowShouldClose(windu))
 	{
+		Input::setDelta(glfwGetTime());
+
 		glfwPollEvents();
 
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
@@ -44,22 +46,13 @@ Game::Game(GLFWwindow *windu)
 
 		//true mvp :'))) will probably abstract to separate class
 
-
-		glm::mat4 vue;
-		glm::mat4 proyek;
-		
-		vue = glm::translate(vue, glm::vec3(0.0f, 0.0f, -3.0f)); //where we are in the world
-
-		proyek = glm::perspective(glm::radians(40.0f), (GLfloat)windowsettings::width / (GLfloat)windowsettings::height, 0.1f, 100.0f);
-
 		//prepare their uniforms to pass to shaders
 		GLint modelLoc = glGetUniformLocation(shades._program, "model");
 		GLint viewLoc = glGetUniformLocation(shades._program, "view");
 		GLint projLoc = glGetUniformLocation(shades._program, "projection");
 
 		//pass to shaders
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(vue));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proyek));
+		G::player.setMatrices(viewLoc, projLoc);
 
 		cuban.rotate((GLfloat)glfwGetTime() * 50.0f, glm::vec3(0.8f, 0.0f, 3.0f));
 		cuban.translate(glm::vec3(0.1f, 0.2f, -5.0f));
@@ -72,7 +65,7 @@ Game::Game(GLFWwindow *windu)
 		triangolo.drawVBO(modelLoc);
 		quaddo.drawEBO(modelLoc);
 		cuban.drawVBO(modelLoc);
-		
+
 		glfwSwapBuffers(windu);
 	}
 
@@ -83,4 +76,72 @@ Game::Game(GLFWwindow *windu)
 Game::~Game()
 {
 
+}
+
+//input shazam==============================
+
+std::vector<bool> Input::_keys = std::vector<bool>(1024);
+GLfloat Input::_lastX = windowsettings::width / 2;
+GLfloat Input::_lastY = windowsettings::height / 2;
+bool Input::_firstMouse = true;
+GLfloat Input::_deltaTime = 0.0f;
+GLfloat Input::_lastFrame = 0.0f;
+
+void Input::InputSetup(GLFWwindow *window)
+{
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+void Input::setDelta(const GLfloat &curFrame)
+{
+	Input::_deltaTime = curFrame - Input::_lastFrame;
+	Input::_lastFrame = curFrame;
+}
+
+void Input::key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << " key was pressed\n";
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			Input::_keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			Input::_keys[key] = false;
+	}
+
+	//All things that use key input==============================
+	G::player.processKeyboard(Input::_keys, Input::_deltaTime);
+}
+
+void Input::scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
+{
+
+	//All things that use scroll input===========================
+	G::player.processMouseScroll(yoffset);
+}
+
+void Input::mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+	if (Input::_firstMouse)
+	{
+		Input::_firstMouse = false;
+		Input::_lastX = xpos;
+		Input::_lastY = ypos;
+	}
+
+	GLfloat xoffset = xpos - Input::_lastX;
+	GLfloat yoffset = Input::_lastY - ypos;
+
+	Input::_lastX = xpos;
+	Input::_lastY = ypos;
+
+	//All things that use mouse movement input===================
+	G::player.processMouseMove(xoffset, yoffset);
 }

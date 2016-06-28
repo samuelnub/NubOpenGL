17965bee0087e03ff4c27ada73f1f2f17e1ae814
samuelnub <notsamnub@gmail.com>
2016-06-28 22:59:33 +0800

    minor update to align naming convention

--- NubOpenGL/NubOpenGL/resources/shaders/colorsFrag.glsl
+++ NubOpenGL/NubOpenGL/resources/shaders/colorsFrag.glsl
@@ -3,8 +3,8 @@
 //declare structs here
 struct Material
 {
-	sampler2D diffuse;
-	sampler2D specular;
+	sampler2D texture_diffuse1;
+	sampler2D texture_specular1;
 	float shininess;
 };
 
@@ -36,7 +36,7 @@ uniform vec3 viewPos;
 void main()
 {
 	//ambient strength
-	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
 
 	//normalise normals to get direct pointing directions, and get light dir by subtracting
 	vec3 norm = normalize(Normal);
@@ -44,13 +44,13 @@ void main()
 
 	//calc diffuse impact
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
 
 	//specular
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords));
 	
 	float distance = length(light.position - FragPos);
 	float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));

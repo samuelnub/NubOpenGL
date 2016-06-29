33902aa1c05e2aa2a5c2d8e98f8084e26f26d820
samuelnub <notsamnub@gmail.com>
2016-06-30 00:28:44 +0800

    translation functions, and maps to hold uniforms to send, no clue if it works lol

--- NubOpenGL/NubOpenGL/model.h
+++ NubOpenGL/NubOpenGL/model.h
@@ -5,6 +5,7 @@
 #include "mesh.h"
 #include <string>
 #include <vector>
+#include <map>
 
 class Model
 {
@@ -14,11 +15,30 @@ private:
 
 	std::vector<Texture> _texturesLoaded;
 
+	glm::vec3 _approxPos;
+	glm::mat4 _modelMat;
+
 public:
+	//lord forgive my public member variables
+	std::map<const GLchar *, glm::vec3> _uniformVec3;
+	std::map<const GLchar *, glm::mat4> _uniformMat4;
+	std::map<const GLchar *, GLfloat> _uniformFloat;
+
+
+
 	Model(GLchar *path);
 	void draw(Shader &shader);
 
 	static GLuint textureFromFile(const char *path, std::string directory);
+
+	void calcApproxPos(); //calcs center based on min. bounding box
+	glm::vec3 getApproxPos();
+
+	void translate(glm::vec3 &translation);
+	void scale(glm::vec3 &scale);
+	void rotate(GLfloat &degrees, glm::vec3 &axis);
+
+	void sendUniforms(Shader &shader);
 
 protected:
 	void loadModel(std::string path);

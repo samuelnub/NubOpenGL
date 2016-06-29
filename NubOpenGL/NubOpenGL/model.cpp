1e0323452689784e617c2c62652a018b4046b562
samuelnub <notsamnub@gmail.com>
2016-06-30 00:30:27 +0800

    yeah didnt work cause of a syntax error you big dumbo

--- NubOpenGL/NubOpenGL/model.cpp
+++ NubOpenGL/NubOpenGL/model.cpp
@@ -269,7 +269,7 @@ void Model::sendUniforms(Shader &shader)
 
 		while (iter != _uniformFloat.end())
 		{
-			glUniform1f(shader._program, iter->second), iter->first);
+			glUniform1f(glGetUniformLocation(shader._program, iter->second), iter->first);
 			//swapped around cause iterator chose the glfloats to be the iterating key
 		}
 	}

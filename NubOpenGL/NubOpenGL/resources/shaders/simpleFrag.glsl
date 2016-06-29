01c953f95c1ecaa32634473e393d9d29bd5e86d2
samuelnub <notsamnub@gmail.com>
2016-06-29 20:44:34 +0800

    modified to suit model loader

--- NubOpenGL/NubOpenGL/resources/shaders/simpleFrag.glsl
+++ NubOpenGL/NubOpenGL/resources/shaders/simpleFrag.glsl
@@ -1,13 +1,12 @@
 #version 330 core
 
-in vec3 ourColor;
-in vec2 TexCoord;
+in vec2 TexCoords;
 
 out vec4 color;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture_diffuse1;
 
 void main()
 {
-	color = texture(ourTexture, TexCoord);
+	color = vec4(texture(texture_diffuse1, TexCoords));
 }
